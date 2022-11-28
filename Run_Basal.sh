036da5f16b1c0cc867f793146d9999ecdbcc7c3c
asaksager <astridsaksager@gmail.com>
2022-11-28 13:53:51 +0100

    Update readme file for folder content

--- Run_Basal.sh
+++ Run_Basal.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-NTHREADS=4
-export OMP_NUM_THREADS=$NTHREADS
-tsp -N $NTHREADS Rscript scripts/run_DMA.R
-#Rscript scripts/run_moonlight.R
-#tsp -N $NTHREADS -L astrid Rscript scripts/run_moonlight.R
-#tsp -N $NTHREADS Rscript scripts/00_init.R
+Rscript scripts/00_init.R
