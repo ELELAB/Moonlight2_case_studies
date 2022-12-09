cc4bf9aaffdd251f6d76e50fbe65a5b19b6cd415
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-12-09 14:10:11 +0100

    refactored package install and load

--- scripts/00_init.R
+++ scripts/00_init.R
@@ -1,15 +1,29 @@
+library(pacman)
+
 # Clear workspace ---------------------------------------------------------
 rm(list = ls())
-
-# Set working directory ---------------------------------------------------
-setwd("./scripts")
 
 # Check all req are install 
 # Check packages ----------------------------------------------------------
 # Checking for the required packages download if not present
-install_if_needed(c("Moonlight2R",
-                    "devtools",
-                    "TCGAbiolinks"))
+
+p_load('BiocManager')
+p_load('osfr')
+p_load('maftools')
+p_load('enrichR')
+p_load('UpSetR')
+p_load('liftOver')
+p_load_gh('ELELAB/Moonlight2R')
+p_load('ggplot2')
+p_load('tidyverse')
+
+# Download data from OSF --------------------------------------------------
+node = osf_retrieve_node('eq9wj')
+files = osf_ls_files(node)
+osf_download(files, recurse=TRUE, conflicts="skip")
+
+# Set working directory ---------------------------------------------------
+setwd("./scripts")
 
 #Run scripts
 
